cc7e5d01f51392e6a1ec5877c094faf833e1a83e
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-12-30 19:12:25 +0600

    Beautiful Matrix

---
+++ others/263A.cpp
@@ -0,0 +1,66 @@
+
+//author:Shakil Ahamed Riaz-(shakilswe)
+//https://codeforces.com/problemset/problem/263/A
+#include <bits/stdc++.h>
+using namespace std;
+
+//for optimizes GCC code
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define ld long double
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626433832795l
+#define pb(e) push_back(e)
+#define fst first
+#define snd second
+#define pr pair<int, int>
+#define mp make_pair
+#define flp(i, a, b) for(int i = a; i <= b; i++)
+#define sqr(a) (a)*(a)
+#define mod 1000000007
+#define N "\n"
+
+using namespace std;
+
+int gcd(int a, int b) { if(a % b == 0) return b; else return gcd(b, a % b); }
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+void sr_sol()
+{
+  
+    int a[5][5];
+
+    int row,col;
+    for(int i=0; i<5; i++)
+    {
+        for(int j=0; j<5; j++)
+        {
+            cin>>a[i][j];
+            if(a[i][j]==1)
+            {
+                row=i;
+                col=j;
+            }
+        }
+    }
+
+    int ans=abs(row-2)+abs(col-2);
+
+    cout<<ans<<N;
+
+}
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    //sr_sol(): shakil_riaz's solution  
+    sr_sol();
+
+    return 0;
+}
