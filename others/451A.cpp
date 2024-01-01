c46c8e7096537d967d1ea3dea6f12f47d3379769
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-01-02 00:47:47 +0600

    dubstep

---
+++ others/451A.cpp
@@ -0,0 +1,62 @@
+//author:Shakil Ahamed Riaz-(shakilswe)
+//https://codeforces.com/problemset/problem/451/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//for optimizes GCC code
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define ld long double
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626433832795l
+#define pb(e) push_back(e)
+#define fst first
+#define snd second
+#define pr pair<int, int>
+#define mp make_pair
+#define flp(i, a, b) for(int i = a; i <= b; i++)
+#define sqr(a) (a)*(a)
+#define mod 1000000007
+#define N "\n"
+
+using namespace std;
+
+int gcd(int a, int b) { if(a % b == 0) return b; else return gcd(b, a % b); }
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+void sr_sol()
+{
+    int n, m;
+    cin >>n>>m;
+
+     int x=1;
+     int cnt=0;
+
+     while(true)
+     {
+        if(n-x==0)break;
+        if(m-x==0)break;
+
+        x+;
+        cnt++;
+     }
+    
+     if(cnt%2==0 ? cout<< "Akshat"<<N : cout<< "Malvika"<<N);
+     
+}
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    //sr_sol(): shakil_riaz's solution  
+    sr_sol();
+
+    return 0;
+}
