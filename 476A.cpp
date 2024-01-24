4c51fecb976f15fd5ee2e42cf16b30361843fed5
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-01-24 22:51:58 +0600

    done

---
+++ 476A.cpp
@@ -0,0 +1,79 @@
+//author:Shakil Ahamed Riaz-(shakilswe)
+#include <bits/stdc++.h>
+using namespace std;
+
+//for optimizes GCC code
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define ld long double
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define PI 3.1415926535897932384626433832795l
+#define pb(e) push_back(e)
+#define fst first
+#define snd second
+#define pr pair<int, int>
+#define mp make_pair
+#define flp(i, a, b) for(int i = a; i <= b; i++)
+#define sqr(a) (a)*(a)
+#define mod 1000000007
+#define N "\n"
+
+using namespace std;
+
+int gcd(int a, int b) { if(a % b == 0) return b; else return gcd(b, a % b); }
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+struct Runtime {
+    clock_t start;
+    Runtime() {
+        start = clock();
+    }
+    ~Runtime() {
+        cout << "Runtime: " << fixed << setprecision(3) << (double) (clock() - start) / CLOCKS_PER_SEC << "s\n";
+    }
+};
+
+void sr_sol()
+{
+  
+  ll n, m, b=0, i=1;
+  cin >>n>>m;
+
+  n=n+1;
+
+  if(m > (n-1))
+  {
+    cout<< "-1" <<N;
+  }
+  else if(m>=n/2)
+  {
+    cout<<m<<N;
+  }
+
+  ll p=(n-1)/2;
+  if(n/2 > m)
+  {
+    while(b<n/2)
+    {
+        b = m*i;
+        i++;
+    }
+
+    cout<<b<<N;
+  }
+
+}
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    //sr_sol(): shakil_riaz's solution  
+    sr_sol();
+    
+    return 0;
+}
