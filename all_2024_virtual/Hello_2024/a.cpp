0e919eecb9d1373328a522f0f4c1757919a2e952
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-04-20 17:14:09 +0600

    another Trap

---
+++ all_2024_virtual/Hello_2024/a.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define ld long double
+#define PI 3.1415926535897932384626433832795
+
+
+using namespace std;
+
+int gcd(int a, int b) { if(a % b == 0) return b; else return gcd(b, a % b); }
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+struct Runtime 
+{
+    clock_t start;
+    Runtime() {
+        start = clock();
+    }
+    ~Runtime() {
+        cout << "Runtime: " << fixed << setprecision(3) << (double) (clock() - start) / CLOCKS_PER_SEC << "s\n";
+    }
+};
+
+void shakil_sol() {
+    
+    int t;
+    cin >>t;
+    while(t--)
+    {
+       ll a, b;
+       cin >>a >>b;
+
+        ll k=a+b;
+
+        if(k%2==0) cout<< "Bob" <<endl;
+        else cout<< "Alice" <<endl;
+
+    }
+}
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    //sr_sol(): shakil_riaz's solution  
+    shakil_sol();
+
+    return 0;
+}
