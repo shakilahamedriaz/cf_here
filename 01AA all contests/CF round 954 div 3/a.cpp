28487aa3ad3b3743c62b9dc8cec108959d5565f8
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-06-23 22:48:59 +0600

    Done

---
+++ 01AA all contests/CF round 954 div 3/a.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#define ll long long
+#define ld long double
+#define PI 3.1415926535897932384626433832795
+using namespace std;
+
+int gcd(int a, int b) { if(a % b == 0) return b; else return gcd(b, a % b); }
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+struct Runtime 
+{
+    clock_t start;
+    Runtime() {
+        start = clock();
+    }
+    ~Runtime() {
+        cout << "Runtime: " << fixed << setprecision(3) << (double) (clock() - start) / CLOCKS_PER_SEC << "s\n";
+    }
+};
+
+
+void shakil_sol() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {        
+        int x, y, z;
+        cin >> x >>y >>z;
+
+        int arr[3] = {x,y,z};
+        sort(arr, arr + 3);
+
+        int temp = arr[1];
+
+        int ans = abs(x-temp) + abs(y-temp) + abs(z-temp);
+
+        cout<< ans << "\n";
+
+    }
+}
+
+
+int32_t main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    shakil_sol();
+
+    return 0;
+}
