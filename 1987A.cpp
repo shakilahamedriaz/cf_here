c16a66de3e46ca003438adf50b5583b56e4ad252
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-07-07 00:17:21 +0600

    cf solving

---
+++ 1987A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#define ll long long
+#define ld long double
+#define PI 3.1415926535897932384626433832795
+
+int gcd(int a, int b) { if(a % b == 0) return b; else return gcd(b, a % b); }
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+struct Runtime {
+    clock_t start;
+    Runtime() {
+        start = clock();
+    }
+    ~Runtime() {
+        cout << "Runtime: " << fixed << setprecision(3) << (double) (clock() - start) / CLOCKS_PER_SEC << "s\n";
+    }
+};
+
+void shakil_sol() 
+{
+    int t;
+    cin >> t;
+    while( t-- )
+    {
+         int n, k;
+         cin >> n >> k;
+
+         int temp = n * k;
+         temp -= (k-1);
+         cout << temp <<  "\n";
+
+    }
+}
+
+int32_t main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    shakil_sol();
+
+    return 0;
+}
+
