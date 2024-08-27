f7b14b35754875c9605f9424cd2988f1ea403cfa
shakilahamedriaz <shakilahamedriaz@gmail.com>
2024-08-27 19:28:25 +0600

    done

---
+++ 01AA all contests/CF round 894 div 3/b.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#define ll long long
+#define ld long double
+#define PI 3.1415926535897932384626433832795
+
+int gcd(int a, int b) { if(a % b == 0) return b; else return gcd(b, a % b); }
+int lcm(int a, int b) { return (a * b) / gcd(a, b); }
+
+struct Runtime {
+    clock_t start;
+    Runtime() {
+        start = clock();
+    }
+    ~Runtime() {
+        cout << "Runtime: " << fixed << setprecision(3) << (double) (clock() - start) / CLOCKS_PER_SEC << "s\n";
+    }
+};
+
+void shakil_sol() 
+{
+  
+      int n, m, it = 0;
+      cin >> n >> m;
+      string s[n], ans = "vika";
+      for(int i = 0; i < n; i++) cin >> s[i];
+
+    
+      for(int i = 0; i < m; i++)
+      {
+        for(int j = 0; j < n; j++)
+        {
+            if(s[j][i] == ans[it])
+            {
+                it++;
+                if(it == 4)
+                {
+                    cout<< "YES" << "\n";
+                    return;
+                }
+                break;
+            }
+        
+         }
+    
+      }
+      cout<< "NO" << "\n";
+
+   }
+
+
+
+int32_t main() 
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        shakil_sol();
+    }
+   
+
+    return 0;
+}
+
